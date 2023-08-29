c9ca8232edcc23a57943f3dbb21345f60a075016
temirovazat <azat.bcraft@gmail.com>
2023-08-29 20:37:47 +0600

    front part of project

--- shop/forms.py
+++ shop/forms.py
@@ -4,6 +4,16 @@ from .models import ShopGoods
 
 
 class GoodsCreateForm(forms.ModelForm):
+    image = forms.ImageField()
+
     class Meta:
         model = ShopGoods
-        fields = ("title", "description", "price", "image", "category")
+        fields = ("title", "description", "price", "image", "category", "from_at")
+        widgets = {
+            'title': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Text area'}),
+            'price': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Price'}),
+            "category": forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Category'}),
+            # "image": forms.ImageField(attrs={'class': 'form-control', 'placeholder': 'Image'}),
+            "from_at": forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'From at'})
+        }
